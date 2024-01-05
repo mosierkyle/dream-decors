971a7e82b8735f78959a4747baffb4904b8b68e4
Kyle Mosier <mosierkyle@gmail.com>
2024-01-04 19:07:46 -0800

    Style individual item

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -7,15 +7,15 @@ const Item = ({ name, image, price, category, back }) => {
       <div className="item-img-div">
         <img className="item-img" src={image} alt={name} />
       </div>
-      <div className="item-info-div">
+      <div className="item-info">
         <p className="item-name">{name}</p>
-        <p className="item-descrption">
+        <p className="item-description">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
           mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien fringilla,
           mattis ligula consectetur, ultrices mauris. Maecenas vitae mattis
           tellus.
         </p>
-        <p className="item-price">{price}</p>
+        <p className="item-price">${price}.99</p>
         <div className="item-purchase"></div>
         <Link to={back}>Go Back</Link>
       </div>
